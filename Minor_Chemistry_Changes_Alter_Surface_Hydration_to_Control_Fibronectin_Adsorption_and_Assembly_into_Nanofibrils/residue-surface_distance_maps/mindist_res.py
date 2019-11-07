2afd73af4ce60c8b14ca95882870f4b0dfbc8527
Mateusz Bieniek <bieniekmat@gmail.com>
2019-11-07 20:39:25 +0000

    Scripts used in during the work on my first publications. First part includes only the calculation of residue-surface distances and then plotting the results in the form of a distance map.

---
+++ Minor_Chemistry_Changes_Alter_Surface_Hydration_to_Control_Fibronectin_Adsorption_and_Assembly_into_Nanofibrils/residue-surface_distance_maps/mindist_res.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Calculate the minimum distance from the heavy atoms of each residue and the surface.
+Save the results in a file with a tabular format with the following format:
+# time(ps) res1aaa.res1id.res1index res2aaa.res2id.res2index
+
+Instructions: read carefuly the script and update the configuration accordingly.
+
+Please cite the following publication if you find this script useful:
+Bieniek, M. K. et al. (2019) ‘Minor Chemistry Changes Alter Surface Hydration to Control Fibronectin Adsorption and Assembly into Nanofibrils’, Advanced Theory and Simulations. John Wiley & Sons, Ltd, p. 1900169. doi: 10.1002/adts.201900169.
+
+Bibtex:
+
+@article{Bieniek2019,
+author = {Bieniek, Mateusz K. and Llopis‐Hernandez, Virginia and Douglas, Katie and Salmeron‐Sanchez, Manuel and Lorenz, Christian D.},
+doi = {10.1002/adts.201900169},
+file = {:home/dresio/Downloads/de/Bieniek{\_}et{\_}al-2019-Advanced{\_}Theory{\_}and{\_}Simulations.pdf:pdf},
+issn = {2513-0390},
+journal = {Advanced Theory and Simulations},
+keywords = {fibrillogenesis,fibronectin,material‐driven fibrillogenesis,molecular dynamics,surface hydration},
+month = {oct},
+pages = {1900169},
+publisher = {John Wiley {\&} Sons, Ltd},
+title = {{Minor Chemistry Changes Alter Surface Hydration to Control Fibronectin Adsorption and Assembly into Nanofibrils}},
+url = {https://onlinelibrary.wiley.com/doi/abs/10.1002/adts.201900169},
+year = {2019}
+}
+
+Please create an issue on github if you need any assistance or have any suggestions.
+"""
+
+import MDAnalysis
+from MDAnalysis.analysis.distances import distance_array
+import numpy as np
+
+# open the trajectory with MDAnalysis
+u = MDAnalysis.Universe('/home/dresio/phd/sams_fn/ea10/500ns_protCent_pbcMol.gro',
+                        '/home/dresio/phd/sams_fn/ea10/500ns_centCA_pbcMol_step100ps.xtc')
+# select the atoms in the substrate that are found on the surface
+substrate = u.select_atoms("resname EA and (name C11 O1 O2 C12 C13)")
+print ('Selected', substrate)
+# the name of the file where the data will be saved
+output_filename = 'data_distance_map.dat'
+
+# select the protein
+protein = u.select_atoms('protein')
+
+# create column titles with the format "time, res1, res2, res3"
+column_titles = ['time(ps)', ]
+# preselect the heavy atoms for each residue (optimisation)
+residues = []
+for i, residue in enumerate(protein.residues, start=1):
+    column_titles.append(residue.resname + '.' + str(residue.resid) + '.' + str(i))
+    residues.append(residue.atoms.select_atoms('not name H*'))
+
+assert len(protein.residues) == len(residues), 'There should be heavy atom indices for each residue'
+
+data = []
+# adjust the step of the trajectory
+for ts in u.trajectory[::10]:
+    row = [ts.time, ]
+    for res_hatoms in residues:
+        # find the minimum distance from the heavy residues to the substrate
+        mindst = np.min(distance_array(res_hatoms.positions, substrate.positions, box=ts.dimensions))
+        row.append(mindst)
+    data.append(row)
+    print ("Done timeframe (ns):", ts.time / 1000)
+
+# save the data with numpy
+np.savetxt(output_filename, data, fmt='%.2f', header=' '.join(column_titles))
---
+++ Minor_Chemistry_Changes_Alter_Surface_Hydration_to_Control_Fibronectin_Adsorption_and_Assembly_into_Nanofibrils/residue-surface_distance_maps/plot_distance_map.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Visualise the distance map of the FnIII 9-10 adsorption to the SAMs calculated
+with a separate script.
+
+
+Please cite the following publication if you find this script useful:
+Bieniek, M. K. et al. (2019) ‘Minor Chemistry Changes Alter Surface Hydration to Control Fibronectin Adsorption and Assembly into Nanofibrils’, Advanced Theory and Simulations. John Wiley & Sons, Ltd, p. 1900169. doi: 10.1002/adts.201900169.
+
+Bibtex:
+
+@article{Bieniek2019,
+author = {Bieniek, Mateusz K. and Llopis‐Hernandez, Virginia and Douglas, Katie and Salmeron‐Sanchez, Manuel and Lorenz, Christian D.},
+doi = {10.1002/adts.201900169},
+file = {:home/dresio/Downloads/de/Bieniek{\_}et{\_}al-2019-Advanced{\_}Theory{\_}and{\_}Simulations.pdf:pdf},
+issn = {2513-0390},
+journal = {Advanced Theory and Simulations},
+keywords = {fibrillogenesis,fibronectin,material‐driven fibrillogenesis,molecular dynamics,surface hydration},
+month = {oct},
+pages = {1900169},
+publisher = {John Wiley {\&} Sons, Ltd},
+title = {{Minor Chemistry Changes Alter Surface Hydration to Control Fibronectin Adsorption and Assembly into Nanofibrils}},
+url = {https://onlinelibrary.wiley.com/doi/abs/10.1002/adts.201900169},
+year = {2019}
+}
+
+Please create an issue on github if you need any assistance or have any suggestions.
+"""
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+
+
+# paths which have to be adjusted by the user
+data_filepath = "data_distance_map.dat"
+plot_output_filepath = "distance_map.png"
+
+# general configuration for matplotlib
+matplotlib.rcParams.update({'font.size': 14})
+plt.figure(figsize=(10, 4))
+# just one plot
+plt.subplot(1, 1, 1)
+plt.title('EA10 Title')
+plt.xlabel('Time (ns)')
+plt.ylabel('Residues')
+
+# read the residue names first
+# this is format dependant and depends on how it was saved in the first place
+resnames = open(data_filepath).readline().split('time(ps)')[1].split()
+resnames = [n.split('.')[0] + ' ' + n.split('.')[1] for n in resnames]
+
+# load the data with numpy
+# plot only every 10th frame (every 10th row), in my case that's a step of 1 ns
+data = np.loadtxt(data_filepath, comments='#')
+assert len(resnames) == data.shape[1] - 1, \
+    'The number of residues does not correspond to the number of data columns ' \
+    '(no of residues + 1 column for time)'
+# remove the time column
+heatmap = data[:, list(range(1, len(resnames) + 1))]
+# the x axis should be time, and the y axis should be residues
+heatmap = heatmap.T
+# plot while ignoring distances above 20 angstroms
+# remember to adjust colour map: jet_r is jet is reversed
+# list of colour maps: https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html
+contactmap = plt.pcolormesh(heatmap, cmap=cm.jet_r, vmin=0, vmax=20)
+
+# prepare places for the yticks (residue names)
+tick_indices = np.array(list(range(0, len(resnames) + 1, 20)))
+# shift each label to apply in the middle of its "square"
+# adjust the fontsize of the y ticks
+plt.yticks(tick_indices + 0.5, [resnames[i] for i in tick_indices], fontsize=12)
+
+# create the legend bar in the figure, the ticks correspond to the distances
+cbar = plt.colorbar(contactmap, ticks=[0, 5, 10, 15, 20], fraction=0.02, pad=0.04)
+cbar.ax.set_yticklabels(['0$\\rm \AA$', '5$\\rm \AA$', '10$\\rm \AA$', '15$\\rm \AA$', '>20$\\rm \AA$'])
+
+plt.tight_layout()
+plt.savefig(plot_output_filepath, dpi=300)
+# plt.show()
+
